e664ae1facfd6addf11f3b859328841d9534ae6b
Daniel Ellard <dellard@bbn.com>
2015-11-05 14:37:02 -0500

    handle context in def lookup

--- src/python/pyqgl2/importer.py
+++ src/python/pyqgl2/importer.py
@@ -10,6 +10,7 @@ import os
 import sys
 
 from pyqgl2.ast_util import NodeError
+from pyqgl2.find_pulse import FindPulseMethods
 from pyqgl2.ast_util import NodeTransformerWithFname
 from pyqgl2.lang import QGL2
 
@@ -37,6 +38,14 @@ class Importer(NodeTransformerWithFname):
         by the imports
     """
 
+    # Names of "Pulse" types.  Note that these are currently
+    # given without scope or context; they are treated like
+    # built-in types rather than classes.  This may change
+    # in the future (i.e. it might be something like
+    # qgl.pulse.Pulse)
+    #
+    PULSE_TYPES = set(['Pulse'])
+
     def __init__(self, module_name):
         super(Importer, self).__init__(module_name)
 
@@ -62,9 +71,13 @@ class Importer(NodeTransformerWithFname):
         # executable statements in the file.  We're glossing over
         # that right now, and assuming it's file-wide and constant.
         #
-        # The format of each entry is path:(path, name)
+        # The format of each entry is path:(imported_path, name)
         #
         self.path2context = dict()
+
+        # The format of each entry is path:{def_name, typename}
+        #
+        self.path2func_defs = dict()
 
         self.context_stack = list()
         self.context_stack.append(list())
@@ -108,17 +121,48 @@ class Importer(NodeTransformerWithFname):
         path to the module that would contain the symbol, but
         doesn't pull out the reference OR check whether
         the reference even exists.
+
+        Returns None if resolution fails.  Otherwise, returns tuple
+        (path, sym_prefix, sym_suffix, (ast, return_type)), where
+
+        path - the name of the file containing the symbol (used as
+            the key to some of the data structures)
+
+        sym_prefix - the name of the module containing the symbol
+
+        sym_suffix - the sym_name, unless the symbol is imported with
+            a different name (not supported yet)
+
+        ast - the AST for the definiton of the symbol
+
+        return_type - the declared return type of the symbol, or None
+            if there is no return annotation
+
         """
 
         sym_components = sym_name.split('.')
         sym_prefix = '.'.join(sym_components[:-1])
+        sym_suffix = sym_components[-1]
 
         context = self.path2context[context_name]
         for path, name in context:
             if name == sym_prefix:
-                return path
+                if self.path2func_defs[path][sym_suffix]:
+                    return (path, sym_prefix, sym_suffix,
+                            self.path2func_defs[path][sym_suffix])
+                else:
+                    break
 
         return None
+
+    def is_pulse(self, context_name, sym_name):
+        lookup = self.resolve_sym(context_name, sym_name)
+
+        if not lookup:
+            return None
+        else:
+            print('GOT %s' % lookup[3][1])
+            return lookup[3][1] in self.PULSE_TYPES
 
     def do_import(self, parent, path, import_name, as_name):
         """
@@ -186,7 +230,11 @@ class Importer(NodeTransformerWithFname):
 
             self.context_stack.append(list())
             self.context_stack[-1].append((path, None))
-            self.visit(ptree)
+
+            print('CREATING FUNC DEFS %s' % path)
+            self.path2func_defs[path] = dict()
+
+            ptree = self.visit(ptree)
 
             self.path2ast[path] = ptree
             self.context_stack.pop()
@@ -232,6 +280,7 @@ class Importer(NodeTransformerWithFname):
             # to pick and choose
             #
             qgl_context = self.context_stack[-1]
+            print('CONTEXT %s' % qgl_context)
             for subnode in ast.walk(stmnt):
                 subnode.qgl_context = qgl_context
 
@@ -242,6 +291,9 @@ class Importer(NodeTransformerWithFname):
         # for the module as the context of the root node
         #
         node.qgl_context = qgl_context
+
+        self.generic_visit(node)
+
         return node
 
     def handle_if(self, node):
@@ -280,6 +332,65 @@ class Importer(NodeTransformerWithFname):
 
         return node
 
+    def visit_FunctionDef(self, node):
+        """
+        Save references to top-level methods, indexed by local
+        name.
+
+        Find methods that are declared to return any type
+        listed in self.PULSE_TYPES, and make note of them
+        """
+
+        # NOTE: we don't inspect nested functions, because
+        # if their parents don't return Pulses, we don't care
+        # whether or not they do.  We need Pulses to be returned
+        # at the surface.
+        #
+        # We also don't care about lambdas (although I'm not
+        # certain whether lambdas can carry annotations anyway).
+        # A method has to be defined with a name, by an ordinary
+        # def statement, before we're interested.
+        #
+        # Final note: if someone uses a decorator to change the
+        # type returned by a method so that it doesn't match the
+        # annotation, we won't detect this.  We ignore decorators
+        # in this test.
+
+        if not node.name:
+            return node
+
+        defname = node.name
+        fname = node.qgl_fname
+
+        # If a method is redefined, treat this as an error
+        #
+        # Alternatively, we could let the last definiton win,
+        # if the two definitions have different signatures the
+        # behavior will be difficult to explain.
+        #
+        if defname in self.path2func_defs[fname]:
+            self.error_msg(node, 'redefinition of %s' % defname)
+            return node
+
+        if node.returns:
+            ret_type = node.returns
+        else:
+            ret_type = None
+
+        # XXX TODO: This is a bit of a hack.  If the return type is an
+        # ast.Name, and has an id that's in self.PULSE_TYPES, then return
+        # the id string.  Otherwise, return and AST.
+        #
+        # We should boil the AST down to a meaningful string, not return
+        # the raw AST.
+        #
+        if (type(ret_type) == ast.Name) and (ret_type.id in self.PULSE_TYPES):
+            ret_type = ret_type.id
+
+        self.path2func_defs[fname][defname] = (node, ret_type)
+
+        return node
+
 
 if __name__ == '__main__':
 
@@ -305,11 +416,23 @@ if __name__ == '__main__':
             print('bailing out 1')
             sys.exit(1)
 
+        pulser = FindPulseMethods(fname)
+        for path in importer.path2ast.keys():
+            node = importer.path2ast[path]
+            pulser.visit(node)
+
         for path in importer.path2ast.keys():
             node = importer.path2ast[path]
             print('GOT NAME %s -> %s' % (path, str(node.qgl_context)))
             print(ast.dump(node))
 
         print('BASENAME %s' % importer.base_fname)
+        print('FUNC_DEF %s' % importer.path2func_defs)
+
+        print('X a.foo %s' % str(importer.resolve_sym('x.py', 'a.foo')))
+        print('X b.bbb %s' % str(importer.resolve_sym('x.py', 'B.bbb')))
+
+        print('Y a.foo %s' % str(importer.is_pulse('x.py', 'a.foo')))
+        print('Y b.bbb %s' % str(importer.is_pulse('x.py', 'B.bbb')))
 
     preprocess(sys.argv[1])
