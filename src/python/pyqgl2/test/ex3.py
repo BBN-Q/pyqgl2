ae28a86460f5f0ceff5a3f11bda3ae4377ea0709
rblazarus <rlazarus@bbn.com>
2016-01-28 15:35:55 -0500

    Here is another examply of "with concur" for qubit initialization.

---
+++ src/python/pyqgl2/test/ex3.py
@@ -0,0 +1,27 @@
+
+# Pull in the symbols that QGL2 uses to embellish the
+# Python3 syntax.  This must be done in every Python
+# file that uses any of these symbols.
+#
+from qgl2.qgl2 import concur, qgl2decl, qgl2main
+from qgl2.qgl2 import classical, pulse, qbit, qbit_list
+from qgl2.qgl2 import Qbit
+
+
+@qgl2decl
+def initq(a:qbit, b:qbit):
+
+    with concur:
+        while MEAS(a):
+            X(a)
+        while MEAS(b):
+            X(b)
+
+
+@qgl2main
+def main():
+
+    q1 = Qbit(1)
+    q2 = Qbit(2)
+    initq(q1,q2)
+    print ("Completed QGL main")
