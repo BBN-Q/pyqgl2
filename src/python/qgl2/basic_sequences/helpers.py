202d3e05c3af1c4c5c9e4072ae020d2e3b48c348
Aaron Helsinger <ahelsing@bbn.com>
2016-02-16 11:31:01 -0500

    rewrote create_cal_seqs so it is good QGL2 (with a simplified QGL1 variant in comments)

--- src/python/qgl2/basic_sequences/helpers.py
+++ src/python/qgl2/basic_sequences/helpers.py
@@ -20,15 +20,57 @@ def create_cal_seqs(qubits: qbit_list, numRepeats, measChans: qbit_list = None, 
     numRepeats = number of times to repeat calibration sequences (int)
     waitcmp = True if the sequence contains branching
     """
-    raise NotImplementedError("Not implemented")
+    # Make all combinations for qubit calibration states for n qubits and repeat
 
     # Original:
-    # if measChans is None:
-    #     measChans = qubits
-
-    # calSet = [Id, X]
-    # # Make all combination for qubit calibration states for n qubits and repeat 
     # calSeqs = [reduce(operator.mul, [p(q) for p,q in zip(pulseSet, qubits)]) for pulseSet in product(calSet, repeat=len(qubits)) for _ in range(numRepeats)]
-
-    # # Add on the measurement operator. 
     # return [[seq, MEAS(*measChans), qwait('CMP')] if waitcmp else [seq, MEAS(*measChans)] for seq in calSeqs] 
+
+    if measChans is None:
+        measChans = qubits
+
+    # Calibrate using Id and X pulses
+    calSet = [Id, X]
+
+    # For QGL1
+#    calSeqs = []
+
+    # Create iterator with the right number of Id and X pulses
+    for pulseSet in product(calSet, repeat=len(qubits)):
+        # Repeat the whole numRepeats times
+        for _ in range(numRepeats):
+            # then do each pulse on each qubit concurrently
+
+            # For QGL1
+ #           seqs = []
+ #           # Get all combinations of the pulses and qubits
+ #           # doing the pulse on the qubit
+ #           for pulse,qubit in zip(pulseSet, qubits):
+ #               seqs.append(pulse(qubit))
+ #           # Do the pulses concurrently for this pulseSet
+ #           calSeqs.append(reduce(operator.mul, seqs))
+
+            # For QGL2
+            # Get all combinations of the pulses and qubits
+            # doing the pulse on the qubit
+            # Do the pulses concurrently for this pulseSet
+            with concur:
+                for pulse,qubit in zip(pulseSet, qubits):
+                    pulse(qubit)
+            # Add on the measurement pulses (done concurrently)
+            with concur:
+                for chan in measChans:
+                    MEAS(chan)
+            # Optionally wait here
+            if waitcmp:
+                qwait('CMP') # How will QGL2 do with this ControlInstruction?
+
+    # QGL1 only here:
+#    # Add on the measurement operator, optionally waiting
+#    newCalSeqs = []
+#    for seq in calSeqs:
+#        if waitcmp:
+#            newCalSeqs.append([seq, MEAS(*measChans), qwait('CMP')])
+#        else:
+#            newCalSeqs.append([seq, MEAS(*measChans)])
+#    return newCalSeqs
