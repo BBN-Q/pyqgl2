1cfbe6a5f9fe6597899e0654141da646ff601b2e
Aaron Helsinger <ahelsing@bbn.com>
2016-02-18 16:00:04 -0500

    include Blake's rewrite of Reset, and then a qgl2 version of that

--- src/python/qgl2/basic_sequences/Feedback.py
+++ src/python/qgl2/basic_sequences/Feedback.py
@@ -14,64 +14,9 @@ from itertools import product
 import operator
 
 # Note that measChans should have a default value that is identical to qubits
-@qgl2decl
-def Reset(qubits: qbit_list, measDelay = 1e-6, signVec = None,
+def Resetq1_orig(qubits: qbit_list, measDelay = 1e-6, signVec = None,
           doubleRound = True, buf = 30e-9, showPlot=False, measChans: qbit_list = None, docals=True, calRepeats=2):
     """
-
-    Variable amplitude Rabi nutation experiment for an arbitrary number of qubits simultaneously
-
-    Parameters
-    ----------
-    qubits : tuple of logical channels to implement sequence (LogicalChannel)
-    measDelay : delay between end of measuerement and LOADCMP
-    signVec : conditions for feedback. List of 0 (flip if signal is above threshold) and 1 (flip if below) for each qubit. Default = 0 for all qubits
-    doubleRound : if true, double round of feedback
-    showPlot : whether to plot (boolean)
-    measChans : tuble of qubits to be measured (LogicalChannel)
-    docals, calRepeats: enable calibration sequences, repeated calRepeats times
-    """
-
-    # Original:
-    # if measChans is None:
-    #     measChans = qubits
-
-    # if signVec == None:
-    #     signVec = [0]*len(qubits)
-
-    # states = create_cal_seqs(qubits,1,measChans=measChans)
-    # FbSet = [Id, X]
-    # FbSet2 = [X, Id]
-    # FbGates = []
-
-    # for count in range(len(qubits)):
-    #     FbGates += [FbSet] if signVec[count]==0 else [FbSet2]
-    # FbSeq = [reduce(operator.mul, [p(q) for p,q in zip(pulseSet, qubits)]) for pulseSet in product(*FbGates)]
-    # seqs = [state + [MEAS(*measChans), Id(qubits[0],measDelay), qwait('CMP'), Id(qubits[0],buf)] + [branch for b in [qif(fbcount,[FbSeq[count]]) for fbcount in range(len(states))] for branch in b] + [MEAS(*measChans)] for count, state in enumerate(states)]
-
-    # if doubleRound:
-    #     seqs = [seq + [Id(qubits[0],measDelay), qwait('CMP'), Id(qubits[0],buf)] + [branch for b in [qif(fbcount,[FbSeq[count]]) for fbcount in range(2**len(qubits))] for branch in b] + [MEAS(*measChans)] for seq in seqs]
-    # print(seqs[0])
-    # if docals:
-    #     seqs += create_cal_seqs(qubits, calRepeats, measChans=measChans)
-
-    # fileNames = compile_to_hardware(seqs, 'Reset/Reset')
-
-    # if showPlot:
-    #     plot_pulse_files(fileNames)
-
-
-    # signVec determines the order that the product(Id, X) sets end up
-    # in
-    # That is, the product() thing means we'll do all combos of Id,X
-    # for each qubit, but we vary the order of those
-    raise NotImplementedError("Reset for QGL2 Not implemented")
-
-# Note that measChans should have a default value that is identical to qubits
-def Resetq1(qubits: qbit_list, measDelay = 1e-6, signVec = None,
-          doubleRound = True, buf = 30e-9, showPlot=False, measChans: qbit_list = None, docals=True, calRepeats=2):
-    """
-
     Variable amplitude Rabi nutation experiment for an arbitrary number of qubits simultaneously
 
     Parameters
@@ -284,3 +229,278 @@ def Resetq1(qubits: qbit_list, measDelay = 1e-6, signVec = None,
     # QGL2 compiler
     compileAndPlot(seqs, 'Reset/Reset', showPlot)
 
+@qgl2decl
+def qresetq2(qubits: qbit_list, signVec, measDelay, buf):
+    # Produces a sequence like:
+    # Id(qubits[0], measDelay)
+    # qwait(CMP)
+    # Id(qubits[0], buf)
+    # 2^numQubits qif calls, 1 per possible comparison value
+    # MEAS*MEAS
+
+    # For each qubit, build the set of feedback actions to perform when
+    # receiving a zero or one in the comparison register
+    # We'll cover all combinations of Id,X on each qubit
+    # ordering Id,X appropriately based on signVec, and will supply
+    # the if clause in the qifs
+
+    # Produce a list of sets of pulses to do concurrently
+    # Each entry in list has numQubits entries
+    # We don't actually evaluate the gates here - do it later when in
+    # the right place
+    # [
+    #     [[gate, q1],[gate2, q2]],
+    #     [[gate, q1], [g,q2]],
+    #     [[gate, q1], [g, q2]]
+    # ]
+    # The goal is to produce
+    # a sequence of concurrent pulses (in each, Id
+    # or X per qubit), of length 2^# qubits
+    # EG for 2 qubits of opposite signVec, it gives:
+    # [Id(q1)*X(q2), Id(q1)*Id(q2), X(q1)*X(q2), X(q1)*Id(q2)]
+    @qgl2decl
+    def makePulsesList(qubits: qbit_list, signVec):
+        FbGates = []
+        for count, q in enumerate(qubits):
+            fbg = []
+            if signVec[count] == 0:
+                for gate in [Id, X]:
+                    fbg.append([gate, q])
+            else: # inverted logic for when qubit state is flipped
+                for gate in [X, Id]:
+                    fbg.append([gate, q])
+            FbGates.append(fbg)
+        FbSeq = []
+        for pulseSet in product(*FbGates):
+            FbSeq.append(pulseSet)
+        return FbSeq
+
+    # Pick the proper pulseSet from the pulsesList
+    # And evaluate the set concurrently
+    @qgl2decl
+    def ifClause(count, pulsesList):
+        with concur:
+            for pq in pulsesList[count]:
+                pq[0](pq[1])
+
+    # Load register
+    Id(qubits[0], measDelay)
+    qwait('CMP')
+    Id(qubits[0], buf)
+
+    # Create a branch for each possible comparison value
+    # Each qif / branch uses a different mask, and the corresponding
+    # pair of gates (created above)
+    for count in range(2**len(qubits)):
+        qif(count, ifClause(count, pulsesList))
+
+    with concur:
+        for q in qubits:
+            MEAS(q)
+
+def qreset(qubits: qbit_list, signVec, measDelay, buf):
+    # Produces a sequence like:
+    # Id(qubits[0], measDelay)
+    # qwait(CMP)
+    # Id(qubits[0], buf)
+    # 2^numQubits qif calls, 1 per possible comparison value
+    # MEAS*MEAS
+
+    # Note that old code did the MEAS on measChans and this is on qubits
+
+    # For each qubit, build the set of feedback actions to perform when
+    # receiving a zero or one in the comparison register
+    FbGates = []
+    # FbSeq will cover all combinations of Id,X on each qubit,
+    # ordering Id,X appropriately based on signVec, and will supply
+    # the if clause in the qifs
+    for count, q in enumerate(qubits):
+        if signVec[count] == 0:
+            FbGates.append([gate(q) for gate in [Id, X]])
+        else: # inverted logic for when qubit state is flipped
+            FbGates.append([gate(q) for gate in [X, Id]])
+
+    FbSeq = []
+    for pulseSet in product(*FbGates):
+        FbSeq.append(reduce(operator.mul, pulseSet))
+#    FbSeq = [reduce(operator.mul, pulseSet) for pulseSet in product(*FbGates)]
+
+    # FbSeq is now a sequence of concurrent pulses (in each, Id
+    # or X per qubit), of length 2^# qubits
+    # EG for 2 qubits of opposite signVec, it gives:
+    # [Id(q1)*X(q2), Id(q1)*Id(q2), X(q1)*X(q2), X(q1)*Id(q2)]
+
+    # Load register
+    seq = [Id(qubits[0], measDelay), qwait('CMP'), Id(qubits[0], buf)]
+
+    # Create a branch for each possible comparison value
+    # Each qif / branch uses a different mask, and the corresponding
+    # pair of gates (created above)
+    for count in range(2**len(qubits)):
+        seq += qif(count, [FbSeq[count]])
+    seq += [MEAS(*qubits)]
+
+    return seq
+
+# This version from Blake who says it fixes bugs in the original
+# Note that measChans should have a default value that is identical to
+# qubits
+@qgl2decl
+def Resetq2(qubits: qbit_list, measDelay = 1e-6, signVec = None,
+           doubleRound = True, buf = 30e-9, showPlot=False,
+           measChans: qbit_list = None, docals=True, calRepeats=2):
+    """
+    Variable amplitude Rabi nutation experiment for an arbitrary number of qubits simultaneously
+
+    Parameters
+    ----------
+    qubits : tuple of logical channels to implement sequence (LogicalChannel)
+    measDelay : delay between end of measuerement and LOADCMP
+    signVec : conditions for feedback. List of 0 (flip if signal is above threshold) and 1 (flip if below) for each qubit. Default = 0 for all qubits
+    doubleRound : if true, double round of feedback
+    showPlot : whether to plot (boolean)
+    measChans : tuble of qubits to be measured (LogicalChannel)
+    docals, calRepeats: enable calibration sequences, repeated calRepeats times
+    """
+    # This will produce 2^numQubits sequences, such that in each
+    # sequence we try a different combination of the bits in the
+    # comparison register.
+    # Each sequence does some calibration-like pulses, measurements,
+    # then a bunch of qifs with all the possible different masks, then
+    # measure again.
+    # And if doubleRound is set, do it again.
+    # This program tests the result of "resetting" starting from all
+    # computational basis states
+
+    # Note that the if clause to the qif depends on the sign of the
+    # qubit: Default is ID, X like in calibration sequences.
+    if measChans is None:
+        measChans = qubits
+
+    # signVec defaults to 0
+    # If it is 1 then the state is 'flipped' and we'll
+    # reverse the corresponding (Id, X) pulses
+    if signVec == None:
+        signVec = [0]*len(qubits)
+    signVec = tuple(signVec)
+
+    initialPulses = [Id, X]
+    initialPulsesList = tuple(product(initialPulses, repeat=len(qubits)))
+
+    @qgl2decl
+    def doOneInitialPulse(pulseSet):
+        # then do each pulse on each qubit concurrently
+        # Get all combinations of the pulses and qubits
+        # doing the pulse on the qubit
+        # Do the pulses concurrently for this pulseSet
+        with concur:
+            for pulse,qubit in zip(pulseSet, qubits):
+                pulse(qubit)
+        # Add on the measurement pulses (done concurrently)
+        with concur:
+            for chan in measChans:
+                MEAS(chan)
+
+    # There will be 2^numQubits sequences
+    for count in range(2**len(qubits)):
+        # Each sequence will start with what looks like a calibration
+        # sequence
+        # Really we're just re-using that function because it creates all
+        # computation basis states followed by measurements.
+
+        # Do the initial pulse for this entry
+        doOneInitialPulse(initialPulsesList[count])
+
+        # After that, each will have a standard block (see qreset)
+        qreset(qubits, signVec, measDelay, buf)
+
+        # If doubling, add to each sequence
+        # The same standard block from above
+        if doubleRound:
+            qreset(qubits, signVec, measDelay, buf)
+
+        # Add a final qwait('CMP') (bug in original implementation of Reset)
+        qwait('CMP')
+
+    # If we're doing calibration too, add that at the very end
+    # - another 2^numQubits * calRepeats sequences
+    if docals:
+        create_cal_seqs(qubits, calRepeats, measChans=measChans)
+
+    # Here we rely on the QGL compiler to pass in the sequence it
+    # generates to compileAndPlot
+    compileAndPlot('Reset/Reset', showPlot)
+
+# This version from Blake who says it fixes bugs in the original
+# Note that measChans should have a default value that is identical to qubits
+def Reset(qubits: qbit_list, measDelay = 1e-6, signVec = None,
+           doubleRound = True, buf = 30e-9, showPlot=False,
+           measChans: qbit_list = None, docals=True, calRepeats=2):
+    """
+    Variable amplitude Rabi nutation experiment for an arbitrary number of qubits simultaneously
+
+    Parameters
+    ----------
+    qubits : tuple of logical channels to implement sequence (LogicalChannel)
+    measDelay : delay between end of measuerement and LOADCMP
+    signVec : conditions for feedback. List of 0 (flip if signal is above threshold) and 1 (flip if below) for each qubit. Default = 0 for all qubits
+    doubleRound : if true, double round of feedback
+    showPlot : whether to plot (boolean)
+    measChans : tuble of qubits to be measured (LogicalChannel)
+    docals, calRepeats: enable calibration sequences, repeated calRepeats times
+    """
+    # This will produce 2^numQubits sequences, such that in each
+    # sequence we try a different combination of the bits in the
+    # comparison register.
+    # Each sequence does some calibration-like pulses, measurements,
+    # then a bunch of qifs with all the possible different masks, then
+    # measure again.
+    # And if doubleRound is set, do it again.
+    # This program tests the result of "resetting" starting from all
+    # computational basis states
+
+    # Note that the if clause to the qif depends on the sign of the
+    # qubit: Default is ID, X like in calibration sequences.
+    if measChans is None:
+        measChans = qubits
+
+    # signVec defaults to 0
+    # If it is 1 then the state is 'flipped' and we'll
+    # reverse the corresponding (Id, X) pulses
+    if signVec == None:
+        signVec = [0]*len(qubits)
+    signVec = tuple(signVec)
+
+    # Collect the final result sequences here
+    seqs = []
+
+    # Each sequence will start with what looks like a calibration
+    # sequence
+    # Really we're just re-using that function because it creates all
+    # computation basis states followed by measurements.
+    # After that, each will have a standard block (see qreset)
+    # There will be 2^numQubits sequences
+    for prep in create_cal_seqs(qubits, 1):
+        seqs.append(
+            prep +
+            [qreset(qubits, signVec, measDelay, buf)]
+            )
+
+    # If doubling, add to each sequence
+    # The same standard block from above
+    if doubleRound:
+        for seq in seqs:
+            seq.append(qreset(qubits, signVec, measDelay, buf))
+
+    # Add a final qwait('CMP') (bug in original implementation of Reset)
+    for seq in seqs:
+        seq.append(qwait('CMP'))
+
+    # If we're doing calibration too, add that at the very end
+    # - another 2^numQubits * calRepeats sequences
+    if docals:
+        seqs += create_cal_seqs(qubits, calRepeats, measChans=measChans)
+
+    # Be sure to un-decorate this function to make it work without the
+    # QGL2 compiler
+    compileAndPlot(seqs, 'Reset/Reset', showPlot)
