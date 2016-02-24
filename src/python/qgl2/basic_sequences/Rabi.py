e07aa94fd1889401e09156364033d7f3156e7461
Aaron Helsinger <ahelsing@bbn.com>
2016-02-24 14:44:13 -0500

    initial versions of the Rabi functions

--- src/python/qgl2/basic_sequences/Rabi.py
+++ src/python/qgl2/basic_sequences/Rabi.py
@@ -6,16 +6,15 @@ from QGL.Compiler import compile_to_hardware
 from QGL.PulseSequencePlotter import plot_pulse_files
 
 from .helpers import create_cal_seqs
+from .new_helpers import compileAndPlot
 
 from functools import reduce
 import operator
 
-from qgl2.qgl2 import qgl2decl, qbit, qbit_list
+from qgl2.qgl2 import qgl2decl, qbit, qbit_list, qgl2main, concur
 
-@qgl2decl
-def RabiAmp(qubit: qbit, amps, phase=0, showPlot=False):
+def RabiAmpq1(qubit: qbit, amps, phase=0, showPlot=False):
     """
-
     Variable amplitude Rabi nutation experiment.
 
     Parameters
@@ -24,12 +23,7 @@ def RabiAmp(qubit: qbit, amps, phase=0, showPlot=False):
     amps : pulse amplitudes to sweep over (iterable)
     phase : phase of the pulse (radians)
     showPlot : whether to plot (boolean)
-
-    Returns
-    -------
-    plotHandle : handle to plot window to prevent destruction
     """
-    raise NotImplementedError("Not implemented")
 
     # Original:
     # seqs = [[Utheta(qubit, amp=amp, phase=phase), MEAS(qubit)] for amp in amps]
@@ -40,10 +34,49 @@ def RabiAmp(qubit: qbit, amps, phase=0, showPlot=False):
     # if showPlot:
     #     plot_pulse_files(fileNames)
 
+    seqs = []
+    for amp in amps:
+        seq = []
+        seq.append(Utheta(qubit, amp=amp, phase=phase))
+        seq.append(MEAS(qubit))
+        seqs.append(seq)
+
+    # Be sure to un-decorate this function to make it work without the
+    # QGL2 compiler
+    compileAndPlot(seqs, 'Rabi/Rabi', showPlot)
+
 @qgl2decl
-def RabiWidth(qubit: qbit, widths, amp=1, phase=0, shapeFun=QGL.PulseShapes.tanh, showPlot=False):
+def RabiAmp(qubit: qbit, amps, phase=0, showPlot=False):
+    """
+    Variable amplitude Rabi nutation experiment.
+
+    Parameters
+    ----------
+    qubit : logical channel to implement sequence (LogicalChannel)
+    amps : pulse amplitudes to sweep over (iterable)
+    phase : phase of the pulse (radians)
+    showPlot : whether to plot (boolean)
     """
 
+    # Original:
+    # seqs = [[Utheta(qubit, amp=amp, phase=phase), MEAS(qubit)] for amp in amps]
+
+    # fileNames = compile_to_hardware(seqs, 'Rabi/Rabi')
+    # print(fileNames)
+
+    # if showPlot:
+    #     plot_pulse_files(fileNames)
+
+    for amp in amps:
+        Utheta(qubit, amp=amp, phase=phase)
+        MEAS(qubit)
+
+    # Here we rely on the QGL compiler to pass in the sequence it
+    # generates to compileAndPlot
+    compileAndPlot('Rabi/Rabi', showPlot)
+
+def RabiWidthq1(qubit: qbit, widths, amp=1, phase=0, shapeFun=QGL.PulseShapes.tanh, showPlot=False):
+    """
     Variable pulse width Rabi nutation experiment.
 
     Parameters
@@ -53,12 +86,7 @@ def RabiWidth(qubit: qbit, widths, amp=1, phase=0, shapeFun=QGL.PulseShapes.tanh
     phase : phase of the pulse (radians, default = 0)
     shapeFun : shape of pulse (function, default = PulseShapes.tanh)
     showPlot : whether to plot (boolean)
-
-    Returns
-    -------
-    plotHandle : handle to plot window to prevent destruction
     """
-    raise NotImplementedError("Not implemented")
 
     # Original: 
     # seqs = [[Utheta(qubit, length=l, amp=amp, phase=phase, shapeFun=shapeFun), MEAS(qubit)] for l in widths]
@@ -69,11 +97,51 @@ def RabiWidth(qubit: qbit, widths, amp=1, phase=0, shapeFun=QGL.PulseShapes.tanh
     # if showPlot:
     #     plot_pulse_files(fileNames)
 
+    seqs = []
+    for l in widths:
+        seq = []
+        seq.append(Utheta(qubit, length=l, amp=amp, phase=phase, shapeFun=shapeFun))
+        seq.append(MEAS(qubit))
+        seqs.append(seq)
+
+    # Be sure to un-decorate this function to make it work without the
+    # QGL2 compiler
+    compileAndPlot(seqs, 'Rabi/Rabi', showPlot)
+
 @qgl2decl
-def RabiAmp_NQubits(qubits: qbit_list, amps, phase=0, showPlot=False,
-                    measChans: qbit_list = None, docals=False, calRepeats=2):
+def RabiWidth(qubit: qbit, widths, amp=1, phase=0, shapeFun=QGL.PulseShapes.tanh, showPlot=False):
+    """
+    Variable pulse width Rabi nutation experiment.
+
+    Parameters
+    ----------
+    qubit : logical channel to implement sequence (LogicalChannel)
+    widths : pulse widths to sweep over (iterable)
+    phase : phase of the pulse (radians, default = 0)
+    shapeFun : shape of pulse (function, default = PulseShapes.tanh)
+    showPlot : whether to plot (boolean)
     """
 
+    # Original: 
+    # seqs = [[Utheta(qubit, length=l, amp=amp, phase=phase, shapeFun=shapeFun), MEAS(qubit)] for l in widths]
+
+    # fileNames = compile_to_hardware(seqs, 'Rabi/Rabi')
+    # print(fileNames)
+
+    # if showPlot:
+    #     plot_pulse_files(fileNames)
+
+    for l in widths:
+        Utheta(qubit, length=l, amp=amp, phase=phase, shapeFun=shapeFun)
+        MEAS(qubit)
+
+    # Here we rely on the QGL compiler to pass in the sequence it
+    # generates to compileAndPlot
+    compileAndPlot('Rabi/Rabi', showPlot)
+
+def RabiAmp_NQubitsq1(qubits: qbit_list, amps, phase=0, showPlot=False,
+                    measChans: qbit_list = None, docals=False, calRepeats=2):
+    """
     Variable amplitude Rabi nutation experiment for an arbitrary number of qubits simultaneously
 
     Parameters
@@ -84,13 +152,7 @@ def RabiAmp_NQubits(qubits: qbit_list, amps, phase=0, showPlot=False,
     showPlot : whether to plot (boolean)
     measChans : tuble of qubits to be measured (LogicalChannel)
     docals, calRepeats: enable calibration sequences, repeated calRepeats times
-
-    Returns
-    -------
-    plotHandle : handle to plot window to prevent destruction
     """
-    raise NotImplementedError("Not implemented")
-
     # Original:
     # if measChans is None:
     #     measChans = qubits
@@ -105,11 +167,70 @@ def RabiAmp_NQubits(qubits: qbit_list, amps, phase=0, showPlot=False,
 
     # if showPlot:
     #     plot_pulse_files(fileNames)
+    if measChans is None:
+        measChans = qubits
+    seqs = []
+    for amp in amps:
+        seq = []
+        pulses = [Utheta(q, amp=amp, phase=phase) for q in qubits]
+        seq.append(reduce(operator.mul, pulses))
+        seq.append(MEAS(*measChans))
+        seqs.append(seq)
+
+    if docals:
+        seqs += create_cal_seqs(qubits, calRepeats, measChans=measChans)
+
+    # Be sure to un-decorate this function to make it work without the
+    # QGL2 compiler
+    compileAndPlot(seqs, 'Rabi/Rabi', showPlot)
 
 @qgl2decl
-def RabiAmpPi(qubit: qbit, mqubit: qbit, amps, phase=0, showPlot=False):
+def RabiAmp_NQubits(qubits: qbit_list, amps, phase=0, showPlot=False,
+                    measChans: qbit_list = None, docals=False, calRepeats=2):
     """
+    Variable amplitude Rabi nutation experiment for an arbitrary number of qubits simultaneously
 
+    Parameters
+    ----------
+    qubits : tuple of logical channels to implement sequence (LogicalChannel)
+    amps : pulse amplitudes to sweep over for all qubits (iterable)
+    phase : phase of the pulses (radians)
+    showPlot : whether to plot (boolean)
+    measChans : tuble of qubits to be measured (LogicalChannel)
+    docals, calRepeats: enable calibration sequences, repeated calRepeats times
+    """
+    # Original:
+    # if measChans is None:
+    #     measChans = qubits
+
+    # seqs = [[reduce(operator.mul, [Utheta(q, amp=amp, phase=phase) for q in qubits]),MEAS(*measChans)] for amp in amps]
+
+    # if docals:
+    #     seqs += create_cal_seqs(qubits, calRepeats, measChans=measChans)
+
+    # fileNames = compile_to_hardware(seqs, 'Rabi/Rabi')
+    # print(fileNames)
+
+    # if showPlot:
+    #     plot_pulse_files(fileNames)
+    if measChans is None:
+        measChans = qubits
+
+    for amp in amps:
+        with concur:
+            for q,ct in zip(qubits, range(len(qubits))):
+                Utheta(q, amp=amp, phase=phase)
+                MEAS(measChans[ct])
+
+    if docals:
+        create_cal_seqs(qubits, calRepeats, measChans=measChans)
+
+    # Here we rely on the QGL compiler to pass in the sequence it
+    # generates to compileAndPlot
+    compileAndPlot('Rabi/Rabi', showPlot)
+
+def RabiAmpPiq1(qubit: qbit, mqubit: qbit, amps, phase=0, showPlot=False):
+    """
     Variable amplitude Rabi nutation experiment.
 
     Parameters
@@ -123,8 +244,6 @@ def RabiAmpPi(qubit: qbit, mqubit: qbit, amps, phase=0, showPlot=False):
     -------
     plotHandle : handle to plot window to prevent destruction
     """
-    raise NotImplementedError("Not implemented")
-
     # Original:
     # seqs = [[X(mqubit),Utheta(qubit, amp=amp, phase=phase), X(mqubit), MEAS(mqubit)] for amp in amps]
 
@@ -135,13 +254,59 @@ def RabiAmpPi(qubit: qbit, mqubit: qbit, amps, phase=0, showPlot=False):
     #     plotWin = plot_pulse_files(fileNames)
     #     return plotWin
 
+    seqs = []
+    for amp in amps:
+        seq = []
+        seq.append(X(mqubit))
+        seq.append(Utheta(qubit, amp=amp, phase=phase))
+        seq.append(X(mqubit))
+        seq.append(MEAS(mqubit))
+        seqs.append(seq)
+
+    # Be sure to un-decorate this function to make it work without the
+    # QGL2 compiler
+    return compileAndPlot(seqs, 'Rabi/Rabi', showPlot)
+
 @qgl2decl
-def SingleShot(qubit: qbit, showPlot = False):
+def RabiAmpPi(qubit: qbit, mqubit: qbit, amps, phase=0, showPlot=False):
+    """
+    Variable amplitude Rabi nutation experiment.
+
+    Parameters
+    ----------
+    qubit : logical channel to implement sequence (LogicalChannel)
+    amps : pulse amplitudes to sweep over (iterable)
+    phase : phase of the pulse (radians)
+    showPlot : whether to plot (boolean)
+
+    Returns
+    -------
+    plotHandle : handle to plot window to prevent destruction
+    """
+    # Original:
+    # seqs = [[X(mqubit),Utheta(qubit, amp=amp, phase=phase), X(mqubit), MEAS(mqubit)] for amp in amps]
+
+    # fileNames = compile_to_hardware(seqs, 'Rabi/Rabi')
+    # print(fileNames)
+
+    # if showPlot:
+    #     plotWin = plot_pulse_files(fileNames)
+    #     return plotWin
+
+    for amp in amps:
+        X(mqubit)
+        Utheta(qubit, amp=amp, phase=phase)
+        X(mqubit)
+        MEAS(mqubit)
+
+    # Here we rely on the QGL compiler to pass in the sequence it
+    # generates to compileAndPlot
+    return compileAndPlot('Rabi/Rabi', showPlot)
+
+def SingleShotq1(qubit: qbit, showPlot = False):
     """
     2-segment sequence with qubit prepared in |0> and |1>, useful for single-shot fidelity measurements and kernel calibration
     """
-    raise NotImplementedError("Not implemented")
-
     # Original:
     # seqs = [[Id(qubit), MEAS(qubit)], [X(qubit), MEAS(qubit)]]
     # filenames = compile_to_hardware(seqs, 'SingleShot/SingleShot')
@@ -149,13 +314,46 @@ def SingleShot(qubit: qbit, showPlot = False):
 
     # if showPlot:
     #     plot_pulse_files(filenames)
+    seqs = [
+        [
+            Id(qubit),
+            MEAS(qubit)
+        ],
+        [
+            X(qubit),
+            MEAS(qubit)
+        ]
+    ]
+
+    # Be sure to un-decorate this function to make it work without the
+    # QGL2 compiler
+    compileAndPlot(seqs, 'SingleShot/SingleShot', showPlot)
 
 @qgl2decl
-def PulsedSpec(qubit: qbit, specOn = True, showPlot = False):
+def SingleShot(qubit: qbit, showPlot = False):
+    """
+    2-segment sequence with qubit prepared in |0> and |1>, useful for single-shot fidelity measurements and kernel calibration
+    """
+    # Original:
+    # seqs = [[Id(qubit), MEAS(qubit)], [X(qubit), MEAS(qubit)]]
+    # filenames = compile_to_hardware(seqs, 'SingleShot/SingleShot')
+    # print(filenames)
+
+    # if showPlot:
+    #     plot_pulse_files(filenames)
+    Id(qubit)
+    MEAS(qubit)
+    X(qubit)
+    MEAS(qubit)
+
+    # Here we rely on the QGL compiler to pass in the sequence it
+    # generates to compileAndPlot
+    compileAndPlot('SingleShot/SingleShot', showPlot)
+
+def PulsedSpecq1(qubit: qbit, specOn = True, showPlot = False):
     """
     Measurement preceded by a qubit pulse if specOn = True
     """
-    raise NotImplementedError("Not implemented")
 
     # Original:
     # qPulse = X(qubit) if specOn else Id(qubit)
@@ -165,6 +363,81 @@ def PulsedSpec(qubit: qbit, specOn = True, showPlot = False):
 
     # if showPlot:
     #     plot_pulse_files(filenames)
+
+    if specOn:
+        seq = [X(qubit)]
+    else:
+        seq = [Id(qubit)]
+    seq.append(MEAS(qubit))
+    seqs = [seq]
+
+    # Be sure to un-decorate this function to make it work without the
+    # QGL2 compiler
+    compileAndPlot(seqs, 'Spec/Spec', showPlot)
+
+@qgl2decl
+def PulsedSpec(qubit: qbit, specOn = True, showPlot = False):
+    """
+    Measurement preceded by a qubit pulse if specOn = True
+    """
+
+    # Original:
+    # qPulse = X(qubit) if specOn else Id(qubit)
+    # seqs = [[qPulse, MEAS(qubit)]]
+    # filenames = compile_to_hardware(seqs, 'Spec/Spec')
+    # print(filenames)
+
+    # if showPlot:
+    #     plot_pulse_files(filenames)
+
+    if specOn:
+        X(qubit)
+    else:
+        Id(qubit)
+    MEAS(qubit)
+
+    # Here we rely on the QGL compiler to pass in the sequence it
+    # generates to compileAndPlot
+    compileAndPlot('Spec/Spec', showPlot)
+
+def Swapq1(qubit: qbit, mqubit: qbit, delays, showPlot=False):
+    """
+    Variable amplitude Rabi nutation experiment.
+
+    Parameters
+    ----------
+    qubit : logical channel to implement sequence (LogicalChannel)
+    amps : pulse amplitudes to sweep over (iterable)
+    phase : phase of the pulse (radians)
+    showPlot : whether to plot (boolean)
+
+    Returns
+    -------
+    plotHandle : handle to plot window to prevent destruction
+    """
+    # Original:
+    # seqs = [[X(qubit), X(mqubit), Id(mqubit, d), MEAS(mqubit)*MEAS(qubit)] for d in delays] + create_cal_seqs((mqubit,qubit), 2, measChans=(mqubit,qubit))
+
+    # fileNames = compile_to_hardware(seqs, 'Rabi/Rabi')
+    # print(fileNames)
+
+    # if showPlot:
+    #     plotWin = plot_pulse_files(fileNames)
+    #     return plotWin
+    seqs = []
+    for d in delays:
+        seq = []
+        seq.append(X(qubit))
+        seq.append(X(mqubit))
+        seq.append(Id(mqubit, d))
+        seq.append(MEAS(mqubit)*MEAS(qubit))
+        seqs.append(seq)
+
+    seqs += create_cal_seqs((mqubit, qubit), 2)
+
+    # Be sure to un-decorate this function to make it work without the
+    # QGL2 compiler
+    return compileAndPlot(seqs, 'Rabi/Rabi', showPlot)
 
 @qgl2decl
 def Swap(qubit: qbit, mqubit: qbit, delays, showPlot=False):
@@ -182,8 +455,6 @@ def Swap(qubit: qbit, mqubit: qbit, delays, showPlot=False):
     -------
     plotHandle : handle to plot window to prevent destruction
     """
-    raise NotImplementedError("Not implemented")
-
     # Original:
     # seqs = [[X(qubit), X(mqubit), Id(mqubit, d), MEAS(mqubit)*MEAS(qubit)] for d in delays] + create_cal_seqs((mqubit,qubit), 2, measChans=(mqubit,qubit))
 
@@ -193,3 +464,70 @@ def Swap(qubit: qbit, mqubit: qbit, delays, showPlot=False):
     # if showPlot:
     #     plotWin = plot_pulse_files(fileNames)
     #     return plotWin
+    for d in delays:
+        X(qubit)
+        X(mqubit)
+        Id(mqubit, d)
+        with concur:
+            MEAS(mqubit)
+            MEAS(qubit)
+
+    cal_seqs((mqubit, qubit), 2)
+
+    # Here we rely on the QGL compiler to pass in the sequence it
+    # generates to compileAndPlot
+    return compileAndPlot('Rabi/Rabi', showPlot)
+
+# Imports for testing only
+from qgl2.qgl2 import Qbit
+from QGL.Channels import Qubit, LogicalMarkerChannel
+import numpy as np
+from math import pi
+
+@qgl2main
+def main():
+    # Set up 2 qbits, following model in QGL/test/test_Sequences
+
+    # FIXME: Cannot use these in current QGL2 compiler, because
+    # a: QGL2 doesn't understand creating class instances, and 
+    # b: QGL2 currently only understands the fake Qbits
+#    qg1 = LogicalMarkerChannel(label="q1-gate")
+#    q1 = Qubit(label='q1', gateChan=qg1)
+#    q1.pulseParams['length'] = 30e-9
+#    q1.pulseParams['phase'] = pi/2
+#    qg2 = LogicalMarkerChannel(label="q2-gate")
+#    q2 = Qubit(label='q2', gateChan=qg2)
+#    q2.pulseParams['length'] = 30e-9
+#    q2.pulseParams['phase'] = pi/2
+
+#    sTrig = LogicalMarkerChannel(label='slaveTrig')
+#    dTrig = LogicalMarkerChannel(label='digitizerTrig')
+#    Mq1 = '';
+#    Mq1gate = LogicalMarkerChannel(label='M-q1-gate')
+#    m = Measurement(label='M-q1', gateChan = Mq1gate, trigChan = dTrig)
+
+#    ChannelLibrary.channelLib = ChannelLibrary.ChannelLibrary()
+#    ChannelLibrary.channelLib.channelDict = {
+#        'q1-gate': qg1,
+#        'q1': q1,
+#        'q2-gate': qg2,
+#        'q2': q2,
+#        'slaveTrig': sTrig,
+#        'digitizerTrig': dTrig,
+#        'M-q1': m,
+#        'M-q1-gate': Mq1gate
+#    }
+#    ChannelLibrary.channelLib.build_connectivity_graph()
+
+    # But the current qgl2 compiler doesn't understand Qubits, only
+    # Qbits. So use that instead when running through the QGL2
+    # compiler, but comment this out when running directly.
+    q1 = Qbit(1)
+    q2 = Qbit(2)
+
+    RabiAmp(q1,  np.linspace(0, 5e-6, 11))
+    RabiWidth(q1,  np.linspace(0, 5e-6, 11))
+    RabiAmp_NQubits((q1, q2), np.linspace(0, 5e-6, 11))
+    RabiAmpPi(q1, q2, np.linspace(0, 5e-6, 11))
+    SingleShot(q1)
+    PulsedSpec(q1)
