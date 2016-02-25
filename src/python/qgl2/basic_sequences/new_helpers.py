dbfea6fc8dfcb606096760b241714e0fbaf92920
Aaron Helsinger <ahelsing@bbn.com>
2016-02-25 13:53:17 -0500

    comment on what init should do

--- src/python/qgl2/basic_sequences/new_helpers.py
+++ src/python/qgl2/basic_sequences/new_helpers.py
@@ -150,7 +150,17 @@ def addCalibration(listOfSequences, tupleOfQubits: qbit_list, numRepeats=2):
 # init will demarcate the beginning of a list of
 # experiments. QGL1 compiler injects WAITs in beginning of
 # every sequence for now
-# FIXME: Figure out what this should do
+# In QGL1 there's an implicit `init_qubits` at the beginning of each sequence.
+# `compile_sequence` adds it [here](https://github.com/BBN-Q/QGL/blob/master/QGL/Compiler.py#L303)
+# by forcing each sequence to wait for a trigger which will come
+# at a long enough interval that the qubit will relax to the ground state.
+# In QGL2 this will not be hidden magic and the programmer should have to call `init`
+# (everywhere you see `seq = []` would probably translate to `init` for now).
+# There is still some discussion needed because the injected wait also serves
+# to synchronize multiple channels and it seems that should still happen automagically for the programmer.
+# FIXME: Figure out what this should do. For now, perhaps it signals the compiler
+# to start a new sequence here and let the QGL1 compile_to_hardware do
+# the original QGL1 thing
 @qgl2decl
 def init(q: qbit):
     pass
