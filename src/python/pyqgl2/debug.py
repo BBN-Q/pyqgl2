d3b70552de6ef1a650ed4c9054f442ce781ec8c2
Daniel Ellard <dellard@bbn.com>
2016-03-03 11:58:58 -0500

    changed defaults and interface
    after using the module for a while, it was obvious that
    the defaults were nonintuitive and difficult to use,
    so changed the interface to something hopefully easier
    to read/understand.

--- src/python/pyqgl2/debug.py
+++ src/python/pyqgl2/debug.py
@@ -12,134 +12,167 @@ printed, and what tags are active/inactive
 
 The "active level" defines the default behavior for
 tags that do not have a specific level assigned.
-All messages that have a level less than or equal to
+All messages that have a level higher than or equal to
 the active level will be printed, unless they have
 a tag with a level that would indicate otherwise.
-The default active level is 0, which means that only
-messages at level 0 will be printed.  The active
+The levels are given mnemonic names with the
+following values:
+
+    NONE = 4
+    HIGH = 3
+    MEDIUM = 2
+    LOW = 1
+    ALL = 0
+
+NONE is a reserved value.
+
+Messages can be assigned any level ALL..HIGH.
+Messages with higher values that HIGH will be
+silently rounded down to HIGH, and messages with
+lower values than ALL will have their levels
+silently rounded up to ALL.
+
+The default active level is NONE, which means
+that no messages will be printed.  The active
 level can be changed via set_level().
 
 The "active tags" defines a mapping between tag names
 and levels.  For example, if the tag 'foo' is bound
-to level 2, then only messages with a level of 2 or
-lower will be printed; all other messages with a tag
-of 'foo' will be discarded.
+to level MEDIUM, then for messages with a tag of
+'foo' only messages with a level of MEDIUM or higher
+will be printed.  (other messages will be filtered
+according to the global active level)
 
 To completely suppress all messages, the active
-level (or the level on a tag) can be set to -1.
+level (or the level on a tag) can be set to NONE.
 
 Examples:
 
-    If 10 is your highest active level, and you want
-    to turn off all but the most urgent messages
-    (i.e. the level 0 messages), but still show all
-    the messages tagged with 'foo':
+    To discard all messages except for those with
+    the tag 'foo', and to print all messages
+    with tag 'foo':
 
-        set_level(0)
-        add_tag('foo', 10)
+        set_level(NONE)
+        add_tag('foo', ALL)
 
-    If 10 is your highest active level, and you want
-    to turn on all messages except those tagged as
-    'foo' (except for those at level 0):
+    To print all messages, except those with tag
+    'foo', which should all be discarded:
 
-        set_level(10)
-        add_tag('foo', 0)
+        set_level(ALL)
+        add_tag('foo', NONE)
 
 """
 
-class DebugMsgState(object):
+class DebugMsg(object):
     """
     Encapsulates the state of the debug message module:
     the active tags, and the active level
     """
 
-    ACTIVE_TAGS = None
-    ACTIVE_LEVEL = 0
+    NONE = 4
+    HIGH = 3
+    MEDIUM = 2
+    LOW = 1
+    ALL = 0
 
-    def __init__():
-        pass
+    ACTIVE_TAGS = dict()
+    ACTIVE_LEVEL = NONE
 
-def add_tag(tag, level):
-    """
-    Add a tag to the set of active tags
 
-    The tag must be a non-empty string; an assertion will be
-    raised on bogus input
+    @staticmethod
+    def reset():
+        DebugMsg.ACTIVE_TAGS = dict()
+        DebugMsg.ACTIVE_LEVEL = DebugMsg.NONE
 
-    It is not an error to activate a tag that is already active
+    @staticmethod
+    def add_tag(tag, level):
+        """
+        Add a tag to the set of active tags
 
-    Note that it doesn't have any effect to add a tag at level 0,
-    since level 0 messages are always printed
-    """
+        The tag must be a non-empty string; an assertion will be
+        raised on bogus input
 
-    assert isinstance(tag, str), 'tag must be a str'
-    assert tag, 'tag must be a non-empty str'
+        It is not an error to activate a tag that is already active
 
-    assert isinstance(level, int), 'level must be an int'
-    assert level >= -1, 'level must be >= -1'
+        Note that it doesn't have any effect to add a tag at level 0,
+        since level 0 messages are always printed
+        """
 
-    if not DebugMsgState.ACTIVE_TAGS:
-        DebugMsgState.ACTIVE_TAGS = dict()
+        assert isinstance(tag, str), 'tag must be a str'
+        assert tag, 'tag must be a non-empty str'
 
-    DebugMsgState.ACTIVE_TAGS[tag] = level
+        assert isinstance(level, int), 'level must be an int'
 
-def set_level(level):
-    """
-    Set the debugging level to the given level
+        # truncate the level, if necessary
+        #
+        if level < DebugMsg.ALL:
+            level = DebugMsg.ALL
+        elif level > DebugMsg.HIGH:
+            level = DebugMsg.HIGH
 
-    The level must be an integer greater than or equal to zero
+        assert level >= -1, 'level must be >= -1'
 
-    Everything at level 0 is printed, but higher levels may
-    be filtered.  There is no fixed assignment to the levels.
-    A common convention is to have a small number of levels
-    (i.e. 0..3) with decreasing levels of verbosity.
-    """
+        DebugMsg.ACTIVE_TAGS[tag] = level
 
-    assert isinstance(level, int), 'level must be an int'
-    assert level >= -1, 'level must be >= -1'
+    @staticmethod
+    def set_level(level):
+        """
+        Set the debugging level to the given level
 
-    old_level = DebugMsgState.ACTIVE_LEVEL
-    DebugMsgState.ACTIVE_LEVEL = level
+        The level must be an integer greater than or equal to zero
+        """
 
-    return old_level
+        assert isinstance(level, int), 'level must be an int'
 
-def debug_msg(msg, level=0, tag=None):
-    """
-    Print a debug msg, labeled with the filename, line number, and
-    function name that invoked debug_msg
+        old_level = DebugMsg.ACTIVE_LEVEL
 
-    Debug messages can be suppressed so that only messages that
-    match one of a set of tags, or that at a level greater than or
-    equal to a global threshold, are printed.
+        # truncate the level, if necessary
+        #
+        if level < DebugMsg.ALL:
+            level = DebugMsg.ALL
+        elif level > DebugMsg.NONE:
+            level = DebugMsg.NONE
 
-    TODO: tags and levels are not supported yet; all messages are
-    printed
-    """
+        DebugMsg.ACTIVE_LEVEL = level
 
-    # level cannot be less than 0; set to 0 if lower.
-    #
-    if level < 0:
-        level = 0
+        return old_level
 
-    # If there's a level for this tag, then use it as the
-    # active level; otherwise, use the general active level
-    #
-    if (tag and DebugMsgState.ACTIVE_TAGS and
-            tag in DebugMsgState.ACTIVE_TAGS):
-        active_level = DebugMsgState.ACTIVE_TAGS[tag]
-    else:
-        active_level = DebugMsgState.ACTIVE_LEVEL
+    @staticmethod
+    def debug_msg(msg, level=ALL, tag=None):
+        """
+        Print a debug msg, labeled with the filename, line number, and
+        function name that invoked debug_msg
 
-    # if the active level is higher than the given level,
-    # then drop the message
-    #
-    if level > active_level:
-        return
+        Debug messages can be suppressed so that only messages that
+        match one of a set of tags, or that at a level greater than or
+        equal to a global threshold, are printed.
+        """
 
-    (filename, lineno, funcname, code) = traceback.extract_stack(limit=2)[0]
+        # truncate the level, if necessary
+        #
+        if level < DebugMsg.ALL:
+            level = DebugMsg.ALL
+        elif level > DebugMsg.HIGH:
+            level = DebugMsg.HIGH
 
-    text = ('DEBUG-%d: %s:%d (%s) %s' %
-            (level, os.path.relpath(filename), lineno, funcname, msg))
+        # If there's a level for this tag, then use it as the
+        # active level; otherwise, use the general active level
+        #
+        if tag and (tag in DebugMsg.ACTIVE_TAGS):
+            active_level = DebugMsg.ACTIVE_TAGS[tag]
+        else:
+            active_level = DebugMsg.ACTIVE_LEVEL
 
-    print('%s' % text)
+        # if the active level is higher than the given level,
+        # then drop the message
+        #
+        if level < active_level:
+            return
+
+        (fname, lineno, funcname, code) = traceback.extract_stack(limit=2)[0]
+
+        text = ('DEBUG-%d: %s:%d (%s) %s' %
+                (level, os.path.relpath(fname), lineno, funcname, msg))
+
+        print('%s' % text)
 
