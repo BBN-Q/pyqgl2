e4b99b809a40ba6ca7807c29b4e1d295df5c7454
Daniel Ellard <dellard@bbn.com>
2016-03-04 09:59:54 -0500

    example input for the current (broken) flattener
    MEAS doesn't do what we want yet; that needs to get
    fixed, or else we need to live with what it does

---
+++ src/python/pyqgl2/test/for/for7.py
@@ -0,0 +1,42 @@
+
+from qgl2.qgl2 import concur, qbit, qgl2decl, Qbit
+from qgl2.qgl2 import Qbit
+
+@qgl2decl
+def MEAS(q: qbit) -> pulse:
+    """
+    measure q, and as a side effect put the result in
+    a magic context-local variable named vmeas.
+    
+    This is rubbish; just for development purposes, because
+    assignment statements like "vmeas = MEAS(q)" trigger
+    a bug in the substition code
+    """
+    measurement(q)
+
+@qgl2decl
+def settle(q: qbit) -> pulse:
+
+    init(q)
+    while True:
+        MEAS(q)
+        if vmeas:
+            break
+        Id(q)
+
+@qgl2decl
+def setup(a: qbit, b: qbit, c: qbit):
+
+    with concur:
+        for qbit in [a, b, c]:
+            settle(qbit)
+
+@qgl2main
+def main():
+
+    x = Qbit(1)
+    y = Qbit(2)
+    z = Qbit(3)
+
+    setup(x, y, z)
+
