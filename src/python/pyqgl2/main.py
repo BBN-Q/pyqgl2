c16764c2105be1dc3d45200de7498c1618a8c38d
Aaron Helsinger <ahelsing@bbn.com>
2016-03-31 11:26:10 -0400

    more helpers to set up channels, make things runnable

--- src/python/pyqgl2/main.py
+++ src/python/pyqgl2/main.py
@@ -255,6 +255,93 @@ def main(filename, main_name=None):
     print('LOCALS %s ' % find_types.local_names)
     """
 
+# Store the given channels in the QGL ChannelLibrary
+def finalize_map(mapping, channels):
+    from QGL import ChannelLibrary
+    for name,value in mapping.items():
+        channels[name].physChan = channels[value]
+
+        ChannelLibrary.channelLib = ChannelLibrary.ChannelLibrary()
+        ChannelLibrary.channelLib.channelDict = channels
+        ChannelLibrary.channelLib.build_connectivity_graph()
+
+# Create a basic channel library
+# Code stolen from QGL's test_Sequences
+def chanSetup(channels=dict()):
+    from QGL.Channels import LogicalMarkerChannel, Measurement, Qubit, PhysicalQuadratureChannel, PhysicalMarkerChannel, Edge
+    from math import pi
+    qubit_names = ['q1','q2']
+    logical_names = ['digitizerTrig', 'slaveTrig']
+
+    for name in logical_names:
+        channels[name] = LogicalMarkerChannel(label=name)
+
+    for name in qubit_names:
+        mName = 'M-' + name
+        mgName = 'M-' + name + '-gate'
+        qgName = name + '-gate'
+
+        mg = LogicalMarkerChannel(label=mgName)
+        qg = LogicalMarkerChannel(label=qgName)
+
+        m = Measurement(label=mName, gateChan = mg, trigChan=channels['digitizerTrig'])
+
+        q = Qubit(label=name, gateChan=qg)
+        q.pulseParams['length'] = 30e-9
+        q.pulseParams['phase'] = pi/2
+
+        channels[name] = q
+        channels[mName] = m
+        channels[mgName]  = mg
+        channels[qgName]  = qg
+
+    # this block depends on the existence of q1 and q2
+    channels['cr-gate'] = LogicalMarkerChannel(label='cr-gate')
+
+    q1, q2 = channels['q1'], channels['q2']
+    cr = Edge(label="cr", source = q1, target = q2, gateChan = channels['cr-gate'] )
+    cr.pulseParams['length'] = 30e-9
+    cr.pulseParams['phase'] = pi/4
+    channels["cr"] = cr
+
+    mq1q2g = LogicalMarkerChannel(label='M-q1q2-gate')
+    channels['M-q1q2-gate']  = mq1q2g
+    channels['M-q1q2']       = Measurement(label='M-q1q2', gateChan = mq1q2g, trigChan=channels['digitizerTrig'])
+
+    # Now assign physical channels
+    for name in ['APS1', 'APS2', 'APS3', 'APS4', 'APS5', 'APS6']:
+        channelName = name + '-12'
+        channel = PhysicalQuadratureChannel(label=channelName)
+        channel.samplingRate = 1.2e9
+        channel.AWG = name
+        channel.translator = 'APS2Pattern'
+        channels[channelName] = channel
+
+        for m in range(1,5):
+            channelName = "{0}-12m{1}".format(name,m)
+            channel = PhysicalMarkerChannel(label=channelName)
+            channel.samplingRate = 1.2e9
+            channel.AWG = name
+            channel.translator = 'APS2Pattern'
+            channels[channelName] = channel
+
+    mapping = {	'digitizerTrig' : 'APS1-12m1',
+                'slaveTrig'     : 'APS1-12m2',
+                'q1'            : 'APS1-12',
+                'q1-gate'       : 'APS1-12m3',
+                'M-q1'          : 'APS2-12',
+                'M-q1-gate'     : 'APS2-12m1',
+                'q2'            : 'APS3-12',
+                'q2-gate'       : 'APS3-12m1',
+                'M-q2'          : 'APS4-12',
+                'M-q2-gate'     : 'APS4-12m1',
+                'cr'            : 'APS5-12',
+                'cr-gate'       : 'APS5-12m1',
+                'M-q1q2'        : 'APS6-12',
+                'M-q1q2-gate'   : 'APS6-12m1'}
+
+    finalize_map(mapping, channels)
+    return channels
 
 if __name__ == '__main__':
     opts = parse_args(sys.argv[1:])
@@ -263,9 +350,19 @@ if __name__ == '__main__':
         # Now import the QGL1 things we need 
         from QGL.Compiler import compile_to_hardware
         from QGL.PulseSequencePlotter import plot_pulse_files
+        import QGL
+        import os
+
+        # Hack. Create a basic channel library
+        channels = chanSetup()
+
+        # Create a directory for saving the results
+        QGL.config.AWGDir = os.path.abspath(QGL.config.AWGDir + os.path.sep + "qgl2main")
+        if not os.path.isdir(QGL.config.AWGDir):
+            os.makedirs(QGL.config.AWGDir)
 
         # Now execute the returned function, which should produce a list of sequences
-        sequences = resFunction()
+        sequences = resFunction(q=channels['q1'])
 
         # Now we have a QGL1 list of sequences we can act on
         fileNames = compile_to_hardware(sequences, opts.prefix, opts.suffix)
