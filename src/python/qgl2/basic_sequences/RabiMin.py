479ff3b17446485d0c5e518a9c0bbbb3593848c3
Aaron Helsinger <ahelsing@bbn.com>
2016-04-11 11:18:58 -0400

    get rid of extra with concur block that is no longer needed

--- src/python/qgl2/basic_sequences/RabiMin.py
+++ src/python/qgl2/basic_sequences/RabiMin.py
@@ -11,27 +11,26 @@ import numpy as np
 @qgl2decl
 def doRabiAmp() -> sequence:
     q = Qubit('q1') # Default qubit that will be replaced
-    with concur: # QGL2 artifact that will be removed
-        # QGL2 cannot yet handle evaluating this itself
+
+    # QGL2 cannot yet handle evaluating this itself
 #        for amp in np.linspace(0,1,11):
-        for amp in [ 0. ,  0.1,  0.2,  0.3,  0.4,  0.5,  0.6,  0.7,  0.8,  0.9,  1. ]:
-            init(q)
-            Utheta(q, amp=amp, phase=0)
-            MEAS(q)
+    for amp in [ 0. ,  0.1,  0.2,  0.3,  0.4,  0.5,  0.6,  0.7,  0.8,  0.9,  1. ]:
+        init(q)
+        Utheta(q, amp=amp, phase=0)
+        MEAS(q)
 
 @qgl2decl
 def doRabiWidth() -> sequence:
     q = Qubit("q1")
-    with concur:
 #        for l in np.linspace(0, 5e-6, 11):
-        for l in [  0.00000000e+00,   5.00000000e-07,   1.00000000e-06,
-                    1.50000000e-06,   2.00000000e-06,   2.50000000e-06,
-                    3.00000000e-06,   3.50000000e-06,   4.00000000e-06,
-                    4.50000000e-06,   5.00000000e-06]:
-            init(q)
-            # FIXME: QGL2 loses the import needed for this QGL function
-            Utheta(q, length=l, amp=1, phase=0, shapeFun=QGL.PulseShapes.tanh)
-            MEAS(q)
+    for l in [  0.00000000e+00,   5.00000000e-07,   1.00000000e-06,
+                1.50000000e-06,   2.00000000e-06,   2.50000000e-06,
+                3.00000000e-06,   3.50000000e-06,   4.00000000e-06,
+                4.50000000e-06,   5.00000000e-06]:
+        init(q)
+        # FIXME: QGL2 loses the import needed for this QGL function
+        Utheta(q, length=l, amp=1, phase=0, shapeFun=QGL.PulseShapes.tanh)
+        MEAS(q)
 
 @qgl2decl
 def doSingleShot() -> sequence:
