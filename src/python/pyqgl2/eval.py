c6282bb9fc588fb2e5f7121a21440be5208914af
Daniel Ellard <dellard@bbn.com>
2016-05-03 16:31:50 -0400

    basic functionality works, but still clunky
    I think this approach has too many limits, and
    we'll need to go back to a hybrid of computing
    constants ahead of time and computing opaque
    objects at runtime.

--- src/python/pyqgl2/eval.py
+++ src/python/pyqgl2/eval.py
@@ -7,6 +7,9 @@ import ast
 from ast import NodeTransformer
 from copy import deepcopy
 
+import pyqgl2.ast_util
+import pyqgl2.inline
+
 from pyqgl2.ast_util import NodeError, ast2str
 from pyqgl2.importer import NameSpaces
 from pyqgl2.single import is_qbit_create
@@ -249,6 +252,63 @@ class SimpleEvaluator(object):
         NodeError.error_msg(node, 'update_node not implemented yet')
         return node
 
+    def do_test(self, node):
+        """
+        Evaluate an test expression, to see if it can be determined
+        in the current context
+        """
+
+        local_variables = self.locals_stack[-1]
+
+        namespace = self.importer.path2namespace[node.qgl_fname]
+        success, val = namespace.native_eval(node, local_variables=local_variables)
+        if not success:
+            NodeError.error_msg(node, 'failed to evaluate [%s]' % ast2str(node))
+            return False, None
+        else:
+            return True, val
+
+    def do_iters(self, node):
+        """
+        Evaluate an iters expression, and see if we can turn it
+        into a list we can express in AST
+        """
+
+        local_variables = self.locals_stack[-1]
+
+        namespace = self.importer.path2namespace[node.qgl_fname]
+        success, val = namespace.native_eval(node, local_variables=local_variables)
+        if not success:
+            NodeError.error_msg(node, 'failed to evaluate [%s]' % ast2str(node))
+            return False, None
+
+        # TODO: review whether this always works.
+        #
+        # If we get something like a set, tuple, iterator, etc, we need to
+        # convert it to a list, because that's the only type we understand
+        # at the next level.  Does just tossing it into a list() do this,
+        # or are there side-effects we need to consider?
+        #
+        if not isinstance(val, list):
+            val = list(val)
+
+        # these are fragile operations; try to be more defensive
+        #
+        try:
+            val_str = str(val)
+            val_ast = ast.parse(val_str, mode='eval')
+        except BaseException as exc:
+            NodeError.error_msg(node, 'cannot convert iter value back to AST')
+            NodeError.error_msg(node, 'original expr: [%s]' % ast2str(node))
+            NodeError.error_msg(node, 'transformed expr: [%s]' % val_str)
+            return False, None
+
+        # What we get back from ast.parse is an ast.Expression, but what
+        # we want is the body wrapped by that Expression
+        #
+        return True, val_ast.body
+
+
     def do_assignment(self, node):
         """
         Handle an assignment statement by creating/updating local bindings
@@ -274,24 +334,208 @@ class SimpleEvaluator(object):
         return True
 
     def do_call(self, call_node):
+        """
+        Incomplete
+        """
 
         funcname = pyqgl2.importer.collapse_name(call_node.func)
         func_ast = self.importer.resolve_sym(call_node.qgl_fname, funcname)
         if not func_ast:
+            # TODO: should this be fatal?  Not sure how we can recover.
+            #
             print('EV NO AST: ERROR')
-            return node
+            return False
 
         # if it's a stub, then leave it alone.
-        if func_ast.qgl_stub:
-            print('EV IS STUB: passing through')
-            return node
+        if pyqgl2.inline.is_qgl2_stub(func_ast):
+            print('EV IS QGL2STUB: passing through')
+            return True
+        elif pyqgl2.inline.is_qgl2_def(func_ast):
+            print('EV IS QGL2DECL: passing through')
+            return True
 
-        # if it's qgl2decl, then we're going to inline it; leave it alone
-        # TODO: something is wrong here because we should never get
-        # a value back from a qgl2decl function; they are called only
-        # for effect.
+        # otherwise, let's see if we can evaluate it.  We do this only
+        # for its side effects; we don't care what the value returned
+        # by the call is (or if there even is one).
 
-        # otherwise, let's see if we can evaluate it.
+        namespace = self.importer.path2namespace[call_node.qgl_fname]
+        print('EV NS %s' % namespace)
+        local_variables = self.locals_stack[-1]
+        success = namespace.native_exec(call_node, local_variables=local_variables)
+        if not success:
+            NodeError.error_msg(call_node,
+                    'failed to evaluate [%s]' % ast2str(call_node))
+            return False
+
+        print('EV locals %s' % str(self.locals_stack[-1]))
+
+        return True
+
+
+class EvalTransformer(object):
+    """
+    Transform an AST by replacing expressions with their values
+    where possible
+
+    This is not a subclass of ast.NodeTransformer because it
+    doesn't implement most of the NodeTransformer interface,
+    and it would be incorrect to use it as a NodeTransformer
+    except in very specific cases.  Rather than try to hobble
+    a NodeTransformer, I'm going to simply implement functions
+    to handle those cases.
+
+    Takes a conservative approach; may need multiple iterations
+    to completely evaluate something.  Needs to work in conjunction
+    with the loop unroller and function inliner; will not attempt
+    to handle loops, stub functions, or qgl2decl'd functions
+    """
+
+    def __init__(self, eval_state):
+        """
+        eval_state is a SimpleEvaluator instance
+        """
+
+        assert isinstance(eval_state, SimpleEvaluator), \
+                ('expected a SimpleEvaluator, got %s' % type(eval_state))
+
+        self.eval_state = eval_state
+
+        # set to zero every time visit() is called, and then
+        # incremented every time a change is made
+        #
+        self.changes_made = 0
+
+    def visit(self, orig_node):
+        """
+        Visit a function definition, transforming it according to
+        the current context, and returning the result.
+
+        Note: this method is called "visit" to match the interface
+        provided by the NodeTransformer classes, but this isn't a
+        NodeTransformer.
+        """
+
+        assert isinstance(orig_node, ast.FunctionDef), \
+                ('expected ast.FunctionDef, got %s' % type(node))
+
+        self.changes_made = 0
+        node = deepcopy(orig_node)
+
+        new_body = list()
+
+        for stmnt_index in range(len(node.body)):
+            stmnt = node.body[stmnt_index]
+
+            # Skip over any pass statements or comment strings.
+            #
+            # TODO we might want to keep some record that we saw them,
+            # but we don't want to emit them.  Right now we discard them.
+            #
+            if isinstance(stmnt, ast.Pass):
+                continue
+            elif (isinstance(stmnt, ast.Expr) and isinstance(stmnt.value, ast.Str)):
+                continue
+
+            elif isinstance(stmnt, ast.Assign):
+                success = self.eval_state.do_assignment(stmnt)
+                print('EV did assignment %s' % str(success))
+
+                # TODO: if we've successfully computed the value of
+                # the assignment, then we don't need it again,
+                # unless it is necessary for a future pass.  Once
+                # we're certain we're done with it, we can get
+                # rid of it -- but this can and should wait until
+                # the final flattening, I think.
+
+                new_body.append(stmnt)
+
+            elif isinstance(stmnt, ast.Expr) and isinstance(stmnt.value, ast.Call):
+                # If it's a call, it could be a function declared to
+                # be a qgl2stub or a qgl2decl, in which case we leave
+                # alone.  We'll attempt to call anything else.
+
+                new_body.append(stmnt)
+
+                success = self.eval_state.do_call(stmnt.value)
+                print('EV did call %s' % str(success))
+
+            elif (isinstance(stmnt, ast.For) and
+                    (not isinstance(stmnt.iter, ast.List))):
+
+                # TODO: if the resulting iter has length 0, then
+                # just the contents of the loop's orelse (if any)
+                # into the new_body.
+
+                new_body.append(stmnt)
+
+                # if it's a for loop, and the iters isn't a list, then
+                # see if we can make progress on this.
+
+                # if the iter is a call to range(), then skip it.
+                # We handle range() as a special case in the unroller
+                # (and if someone redefines range(), then they're
+                # in trouble!)
+                #
+                # TODO: is there any other way to reference range()?
+                # For example, via builtins?
+                #
+                if (isinstance(stmnt.iter, ast.Call) and
+                        (stmnt.iter.func.id == 'range')):
+                    continue
+
+                success, new_iter = self.eval_state.do_iters(stmnt.iter)
+                if success:
+                    print('EV changed %s to %s' %
+                            (ast2str(stmnt.iter), ast2str(new_iter)))
+
+                    stmnt.iter = new_iter
+                    self.changes_made += 1
+
+            elif isinstance(stmnt, ast.If):
+                # if is't an "if" statement, try to figure out
+                # whether the condition is determined, and if so
+                # the replace this statement with either the "if"
+                # or "else" clause.
+
+                success, test = self.eval_state.do_test(stmnt.test)
+                if success:
+                    if test:
+                        stmnt_list = stmnt.body
+                    else:
+                        stmnt_list = stmnt.orelse
+
+                    # TODO: skip over pass statements, or anything after
+                    # a 'break' or 'continue' statement.
+                    for substmnt in stmnt_list:
+                        if isinstance(substmnt, ast.Pass):
+                            continue
+
+                        print('EV peval [%s]' % ast.dump(substmnt))
+                        new_body.append(substmnt)
+                        print('EV peval %s' % ast2str(substmnt))
+
+            else:
+                # TODO: we could add more sanity checks here,
+                # looking for things that shouldn't ever happen.
+                # Right now we just assume that anything else is OK,
+                # but there are things that could go wrong.
+
+                new_body.append(stmnt)
+                print('EV unhandled [%s]' % ast.dump(stmnt))
+
+        # If we've pruned everything out of the body, 
+        # then insert a pass statement to keep things
+        # syntactically correct
+        #
+        if len(new_body) == 0:
+            new_pass = ast.Pass()
+            pyqgl2.ast_util.copy_all_loc(new_pass, orig_node)
+            new_body.append(new_pass)
+
+        node.body = new_body
+
+        print('EV final\n%s' % ast2str(node))
+        return node
 
 
 if __name__ == '__main__':
@@ -331,5 +575,8 @@ if __name__ == '__main__':
     for stmnt in ptree.body:
         if isinstance(stmnt, ast.Assign):
             evaluator.do_assignment(stmnt)
+
+    et = EvalTransformer(SimpleEvaluator(importer, None))
+    ptree2 = et.visit(ptree)
 
 
