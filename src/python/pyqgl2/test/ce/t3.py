c34cff6f4079aa3ddb2d5961765cb3aa0d1df688
Daniel Ellard <dellard@bbn.com>
2016-05-19 18:55:35 -0400

    drafts of tests for different interesting cases

---
+++ src/python/pyqgl2/test/ce/t3.py
@@ -0,0 +1,26 @@
+
+from qgl2.qgl2 import concur, qbit, qgl2decl
+from qgl2.qgl2 import sequence, pulse, qgl2main, classical
+
+from qgl2.qgl1 import QubitFactory, X90, Y90, X, MEAS, Utheta, Xtheta
+from qgl2.qgl1 import Call, BlockLabel, Goto, LoadRepeat, Repeat, Return
+from qgl2.qgl1 import CmpEq
+
+from qgl2.util import init
+
+@qgl2main
+def main():
+
+    x0 = QubitFactory('0')
+    x1 = QubitFactory('1')
+
+    with concur:
+        while not MEAS(x0):
+            X(x0)
+
+    with concur:
+        while not MEAS(x1):
+            X(x1)
+
+
+
