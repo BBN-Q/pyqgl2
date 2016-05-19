a661828fd4fec56a8c62418c841267239f584bd3
Daniel Ellard <dellard@bbn.com>
2016-05-19 14:09:38 -0400

    move init() to its own file
    this breaks circular dependencies between qgl2 and qgl1

--- src/python/qgl2/qgl2.py
+++ src/python/qgl2/qgl2.py
@@ -12,8 +12,6 @@ from qgl2.qgl2 import classical, pulse, qbit, qbit_list, sequence, control, GATH
 from qgl2.qgl1 import *
 """
 
-from qgl2.qgl1 import Sync, Wait
-
 class SimpleWithObject(object):
     """
     Base class that defines a degenerate __enter__ and __exit__
@@ -155,27 +153,3 @@ control = True
 concur = Concur()
 seq = Seq()
 
-# init will demarcate the beginning of a list of
-# experiments. QGL1 compiler injects WAITs in beginning of
-# every sequence for now
-# In QGL1 there's an implicit `init_qubits` at the beginning of each sequence.
-# `compile_sequence` adds it [here](https://github.com/BBN-Q/QGL/blob/master/QGL/Compiler.py#L303)
-# by forcing each sequence to wait for a trigger which will come
-# at a long enough interval that the qubit will relax to the ground state.
-# In QGL2 this will not be hidden magic and the programmer should have to call `init`
-# (everywhere you see `seq = []` would probably translate to `init` for now).
-# There is still some discussion needed because the injected wait also serves
-# to synchronize multiple channels and it seems that should still happen automagically for the programmer.
-# There will be multiple ways to call init() and the programmer must choose.
-# init is the marker of a new sequence
-
-@qgl2decl
-def init(q: qbit) -> pulse:
-    """
-    Sync() and then Wait()
-
-    Annotated as returning a pulse for backwards compatibility.
-    """
-
-    Sync()
-    Wait()
