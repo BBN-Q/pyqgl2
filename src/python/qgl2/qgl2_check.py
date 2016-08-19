cafe6269726ce6481a33f9aa2bcd854c87fe38c9
Daniel Ellard <dellard@bbn.com>
2016-08-19 10:24:08 -0400

    prototype of the QGL2check procedure
    for whatever reason, sys.exit isn't causing things to exit
    immediately.  Not sure why that is.

---
+++ src/python/qgl2/qgl2_check.py
@@ -0,0 +1,21 @@
+# Copyright 2016 by Raytheon BBN Technologies Corp.  All Rights Reserved.
+
+import sys
+
+from pyqgl2.inline import QubitPlaceholder
+
+def QGL2check(value, required_type, fp_name, fname, lineno, colno):
+
+    if required_type == 'qbit':
+        if not isinstance(value, QubitPlaceholder):
+            print('%s:%d:%d: error: bad type: [%s] must be a qbit' %
+                    (fname, lineno, colno, fp_name))
+            sys.exit(1)
+
+    elif required_type == 'classical':
+        if isinstance(value, Qubit):
+            print('%s:%d:%d: error: bad type: [%s] must be classical' %
+                    (fname, lineno, colno, fp_name))
+            sys.exit(1)
+
+    return True
