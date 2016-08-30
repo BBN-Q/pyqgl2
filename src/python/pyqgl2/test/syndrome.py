e308dd01ef23bb10ad5873c871acd6675670dad1
Daniel Ellard <dellard@bbn.com>
2016-08-30 18:29:29 -0400

    sketch of syndrome calculation
    the syndrome class is simplistic and inelegant,
    but this is a drafting exercise to make sure that
    the language features work

--- src/python/pyqgl2/test/syndrome.py
+++ src/python/pyqgl2/test/syndrome.py
@@ -1,26 +1,29 @@
-from qgl2.qgl2 import qgl2decl, qgl2main
-from qgl2.qgl2 import qbit, classical
-from qgl2.qgl2 import concur
-from qgl2.qgl1 import QubitFactory
-from qgl2.qgl1 import X, Y, Id
+from qgl2.qgl2 import qgl2decl, qgl2main, qbit, concur
+from qgl2.qgl1 import QubitFactory, X, Y, Z
 from qgl2.qgl2_check import QGL2check
 
-class _SyndromeRole(object):
+class SyndromeRole(object):
 
     def __init__(self, qbit_type, neighbors):
-        self.x = False
-        self.z = False
 
-        if qbit_type == 'x':
-            self.x = True
-        elif qbit_type == 'z':
-            self.z = True
+        # half-hearted error checking
+        #
+        # We don't check that the neighbors are bogus
+        #
+        assert qbit_type in ['x', 'z', 'd'], \
+                ('invalid qbit_type [%s]' % str(qbit_type))
+        assert isinstance(neighbors, list)
+        assert len(neighbors) == 4
 
+        self.qbit_type = qbit_type
         self.neighbors = list(neighbors)
 
-
-def make_role(qbit_type, neighbors):
-    return _SyndromeRole(qbit_type, neighbors)
+# Completely fake Hadamard and CNOT operations;
+# placeholders for the real operations
+#
+@qgl2decl
+def H(q: qbit):
+    Z(q)
 
 @qgl2decl
 def CNOT(q1: qbit, q2: qbit):
@@ -28,9 +31,13 @@ def CNOT(q1: qbit, q2: qbit):
         X(q1)
         Y(q2)
 
-
 @qgl2decl
 def syndrome_cycle(qbits, role_def):
+
+    with concur:
+        for q in qbits:
+            if role_def[q].qbit_type == 'x':
+                H(q)
 
     for direction in range(4):
         with concur:
@@ -39,15 +46,20 @@ def syndrome_cycle(qbits, role_def):
                 neighbor = role.neighbors[direction]
 
                 if neighbor:
-                    if role.x:
+                    if role.qbit_type == 'x':
                         CNOT(q, neighbor)
-                    elif role.z:
+                    elif role.qbit_type == 'z':
                         CNOT(neighbor, q)
+
+    with concur:
+        for q in qbits:
+            if role_def[q].qbit_type == 'x':
+                H(q)
 
 @qgl2main
 def main():
     """
-    The initial role is:
+    The initial role of each qbit is:
 
     q0:d  q1:x  q2:d
     q3:z  q4:d  q5:z
@@ -71,14 +83,26 @@ def main():
     all_qbits = [q0, q1, q2, q3, q4, q5, q6, q7, q8]
 
     role_def = dict()
-    role_def[q0] = make_role('d', [None, None, q1, q3])
-    role_def[q1] = make_role('x', [None, q0, q2, q4])
-    role_def[q2] = make_role('d', [None, q1, None, q5])
-    role_def[q3] = make_role('z', [q0, None, q4, q6])
-    role_def[q4] = make_role('d', [q1, q3, q5, q7])
-    role_def[q5] = make_role('z', [q2, q4, None, q8])
-    role_def[q6] = make_role('d', [q3, None, q7, None])
-    role_def[q7] = make_role('x', [q4, q6, q8, None])
-    role_def[q8] = make_role('d', [q5, q7, None, None])
+    role_def[q0] = SyndromeRole('d', [None, None, q1, q3])
+    role_def[q1] = SyndromeRole('x', [None, q0, q2, q4])
+    role_def[q2] = SyndromeRole('d', [None, q1, None, q5])
+    role_def[q3] = SyndromeRole('z', [q0, None, q4, q6])
+    role_def[q4] = SyndromeRole('d', [q1, q3, q5, q7])
+    role_def[q5] = SyndromeRole('z', [q2, q4, None, q8])
+    role_def[q6] = SyndromeRole('d', [q3, None, q7, None])
+    role_def[q7] = SyndromeRole('x', [q4, q6, q8, None])
+    role_def[q8] = SyndromeRole('d', [q5, q7, None, None])
+
+    #role_def = {
+    #        q0 : SyndromeRole('d', [None, None, q1, q3]),
+    #        q1 : SyndromeRole('x', [None, q0, q2, q4]),
+    #        q2 : SyndromeRole('d', [None, q1, None, q5]),
+    #        q3 : SyndromeRole('z', [q0, None, q4, q6]),
+    #        q4 : SyndromeRole('d', [q1, q3, q5, q7]),
+    #        q5 : SyndromeRole('z', [q2, q4, None, q8]),
+    #        q6 : SyndromeRole('d', [q3, None, q7, None]),
+    #        q7 : SyndromeRole('x', [q4, q6, q8, None]),
+    #        q8 : SyndromeRole('d', [q5, q7, None, None]),
+    #}
 
     syndrome_cycle(all_qbits, role_def)
