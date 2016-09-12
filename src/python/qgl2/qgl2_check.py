b880f02b6abe665fa8c3f300b0a0e674620a3488
Daniel Ellard <dellard@bbn.com>
2016-09-12 16:56:02 -0400

    new combination checks (not referenced yet)
    do checks in sequence before seeing if any of them
    failed; give the programmer more info about what went
    wrong

--- src/python/qgl2/qgl2_check.py
+++ src/python/qgl2/qgl2_check.py
@@ -7,6 +7,13 @@ from pyqgl2.ast_util import NodeError
 
 def QGL2check(value, required_type, fp_name, fun_name, fname, lineno, colno):
 
+    assert isinstance(required_type, str), 'required_type must be a str'
+    assert isinstance(fp_name, str), 'fp_name must be a str'
+    assert isinstance(fun_name, str), 'fun_name must be a str'
+    assert isinstance(fname, str), 'fname must be a str'
+    assert isinstance(lineno, int), 'lineno must be an int'
+    assert isinstance(colno, int), 'colno must be a int'
+
     if required_type == 'qbit':
         if not isinstance(value, QubitPlaceholder):
             print(('%s:%d:%d: error: ' +
@@ -20,5 +27,25 @@ def QGL2check(value, required_type, fp_name, fun_name, fname, lineno, colno):
                 'param [%s] of func [%s] must be classical') %
                     (fname, lineno, colno, fp_name, fun_name))
             NodeError.MAX_ERR_LEVEL = NodeError.NODE_ERROR_ERROR
+
+    return True
+
+def QGL2check_vec(checks):
+    """
+    Combine multiple checks (from a vector of checks) so all the
+    checks are performed, even if one or more of them fail.
+    This is useful because typically when a programmer makes
+    one type mistake, they make several, so we might as well try
+    to tell them about as many as possible instead of exiting
+    after detecting the first error.
+
+    Each check is a tuple that will be used as the positional
+    parameters to QGL2check.  QGL2check makes an attempt to
+    validate its parameters and aborts the program if it detects
+    problems.
+    """
+
+    for check in checks:
+        QGL2check(*check)
 
     return True
