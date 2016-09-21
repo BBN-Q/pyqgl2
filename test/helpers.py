5c76ad62ff8f8eaf65da5717506e20d98578ec7e
Aaron Helsinger <ahelsing@bbn.com>
2016-09-21 11:33:16 -0400

    Add flattening of sequences to make them testable, so we can support echoCR

--- test/helpers.py
+++ test/helpers.py
@@ -4,7 +4,9 @@ Utilities for creating a basic channel configuration for testing.
 
 from pyqgl2.main import mapQubitsToSequences
 from pyqgl2.evenblocks import replaceBarriers
-from QGL.PulseSequencer import Pulse
+from QGL.PulseSequencer import Pulse, CompositePulse
+from QGL.PatternUtils import flatten
+import collections
 
 def discard_zero_Ids(seqs):
     # assume seqs has a structure like [[entry0, entry1, ..., entryN]]
@@ -17,12 +19,33 @@ def discard_zero_Ids(seqs):
             else:
                 ct += 1
 
+# Things like echoCR create lists of pulses that need to be flattened
+# before calling compile_to_hardware
+def flattenSeqs(seqs):
+    nseqs = []
+    for seq in seqs:
+        hasList = False
+        for el in seq:
+            if isinstance(el, collections.Iterable) and not isinstance(el, (str, Pulse, CompositePulse)) :
+                hasList = True
+                break
+        if hasList:
+            newS = []
+            for el in flatten(seq):
+                newS.append(el)
+            nseqs.append(newS)
+        else:
+            nseqs.append(seq)
+    return nseqs
+
 def testable_sequence(seqs):
     '''
     Transform a QGL2 result function output into something more easily testable,
-    by replacing barriers and discarding zero length Id's.
+    by replacing barriers and discarding zero length Id's and
+    flattening pulse lists.
     '''
     seqIdxToChannelMap, _ = mapQubitsToSequences(seqs)
     seqs = replaceBarriers(seqs, seqIdxToChannelMap)
     discard_zero_Ids(seqs)
+    seqs = flattenSeqs(seqs)
     return seqs
