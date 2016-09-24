aad5b4c678ff6da9931bbea524699a2be4756938
Daniel Ellard <dellard@bbn.com>
2016-09-24 14:39:03 -0400

    add the QGL impl of flat_top_gaussian_impl

--- src/python/qgl2/qgl1_util.py
+++ src/python/qgl2/qgl1_util.py
@@ -2,10 +2,19 @@
 
 # TODO this should be part of QGL, not in QGL2
 
+from QGL.ChannelLibrary import EdgeFactory
 from QGL.ControlFlow import Sync, Wait
+from QGL.PulsePrimitives import flat_top_gaussian
 
 def init_real(q):
     return [
         Sync(),
         Wait()
     ]
+
+def flat_top_gaussian_edge_impl(
+        source, target, riseFall, length, amp, phase=0):
+
+    CRchan = EdgeFactory(source, target)
+    return flat_top_gaussian(CRchan, riseFall, length, amp, phase=0)
+
