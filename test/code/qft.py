a37e616fcb0824ece4c02772f2a271b9aa080947
Blake R. Johnson <bjohnson@bbn.com>
2016-10-24 15:01:56 -0400

    Add QFT test.

---
+++ test/code/qft.py
@@ -0,0 +1,28 @@
+from qgl2.qgl2 import concur, qgl2decl, qgl2main
+from qgl2.qgl2 import classical, pulse, qbit, qbit_list
+from qgl2.qgl1 import QubitFactory, Id, X90, Y90, X, Y, Ztheta, MEAS, CNOT_CR
+
+from math import pi
+
+@qgl2decl
+def hadamard(q:qbit):
+    Y90(q)
+    X(q)
+
+@qgl2decl
+def CZ_k(c:qbit, t:qbit, k):
+    theta = 2 * pi / 2**k
+    Ztheta(t, angle=theta/2)
+    CNOT_CR(c, t)
+    Ztheta(t, angle=-theta/2)
+    CNOT_CR(c, t)
+
+@qgl2decl
+def qft(qs:qbit_list):
+    for i in range(len(qs)):
+        hadamard(qs[i])
+        for j in range(i+1, len(qs)):
+            CZ_k(qs[i], qs[j], j-i)
+    with concur:
+        for q in qs:
+            MEAS(q)
---
+++ test/test_qft.py
@@ -0,0 +1,46 @@
+# Copyright 2016 by Raytheon BBN Technologies Corp.  All Rights Reserved.
+
+import unittest
+
+# Test functions in multi.py
+
+from pyqgl2.main import compileFunction
+from test.helpers import channel_setup, testable_sequence
+
+from test.helpers import testable_sequence, discard_zero_Ids, \
+    flattenSeqs, channel_setup, assertPulseSequenceEqual, \
+    get_cal_seqs_1qubit, get_cal_seqs_2qubits
+
+from QGL import *
+
+class TestQFT(unittest.TestCase):
+    def setUp(self):
+        channel_setup()
+
+    def tearDown(self):
+        pass
+
+    def test_qft(self):
+        q1 = QubitFactory('q1')
+        q2 = QubitFactory('q2')
+        qs = [q1, q2]
+
+        resFunction = compileFunction('test/code/qft.py', 'qft', (qs,))
+        seqs = resFunction()
+        seqs = testable_sequence(seqs)
+
+        # expected_seq = H(q1) + CZ_k(q1, q2, pi) + H(q2) + [MEAS(q1)*MEAS(q2)]
+        expected_seq0 = H(q1) + \
+                        CNOT_CR(q1, q2) + CNOT_CR(q1, q2) + \
+                        [Id(q1, length=60e-9)] + \
+                        [MEAS(q1)]
+        expected_seq1 = [Id(q2, length=60e-9)] + \
+                        [Ztheta(q2, pi/2)] + CNOT_CR(q1, q2) + [Ztheta(q2, -pi/2)] + CNOT_CR(q1, q2) + \
+                        H(q2) + \
+                        [MEAS(q2)]
+
+        assertPulseSequenceEqual(self, seqs[0], expected_seq0)
+        assertPulseSequenceEqual(self, seqs[1], expected_seq1)
+
+def H(q):
+    return [Y90(q), X(q)]
