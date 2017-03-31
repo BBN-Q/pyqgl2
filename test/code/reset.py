5ac645d651444613745ca65e3c3473324a1a4b41
Blake Johnson <bjohnson@bbn.com>
2017-03-31 10:35:54 -0400

    Automatically import control flow symbols used by the flattener.

--- test/code/reset.py
+++ test/code/reset.py
@@ -2,11 +2,6 @@ from qgl2.qgl2 import concur, qgl2decl, qgl2main
 from qgl2.qgl2 import classical, pulse, qbit, qbit_list
 from qgl2.qgl1 import QubitFactory, Id, X90, Y90, X, Y, Z, MEAS
 
-# FIXME these are inserted by the flattener, the user shouldn't have to
-# import them manually
-from qgl2.qgl1 import LoadCmp, CmpEq, CmpNeq, CmpGt, CmpLt, Goto
-from qgl2.qgl1 import BlockLabel, Store
-
 @qgl2decl
 def reset1():
     q = QubitFactory('q1')
