7a723075e6434424a14dcf042a5b48c0383da6e8
Blake R. Johnson <blake.johnson@raytheon.com>
2017-04-07 16:51:46 -0400

    Add basic QRegister tests.

---
+++ test/test_qregister.py
@@ -0,0 +1,44 @@
+import unittest
+
+import ast
+from pyqgl2.qreg import QRegister, QReference
+
+class TestQRegister(unittest.TestCase):
+    def setUp(self):
+        QRegister.reset()
+
+    def test_reg_width(self):
+        a = QRegister(4)
+        self.assertEqual(len(a), 4)
+        self.assertEqual(a.qubits, [1,2,3,4])
+
+    def test_reg_names(self):
+        a = QRegister("q1", "q3")
+        self.assertEqual(len(a), 2)
+        self.assertEqual(a.qubits, [1, 3])
+
+    def test_reg_concat(self):
+        a = QRegister(2)
+        b = QRegister(3)
+        c = QRegister(a, b)
+        self.assertEqual(len(c), len(a) + len(b))
+        self.assertEqual(c.qubits, a.qubits + b.qubits)
+
+    def test_reg_ordering(self):
+        a = QRegister("q2")
+        b = QRegister(3)
+        self.assertEqual(len(b), 3)
+        self.assertEqual(b.qubits, [1,3,4])
+
+    def test_qref(self):
+        a = QRegister(3)
+        b = a[2]
+        self.assertTrue(isinstance(b, QReference))
+        self.assertEqual(b.ref, a)
+        self.assertEqual(b.idx, 2)
+
+    def test_factory(self):
+        node = ast.parse("a = QRegister('q1', 'q5')").body[0]
+        a = QRegister.factory(node, {})
+        self.assertEqual(len(a), 2)
+        self.assertEqual(a.qubits, [1,5])
